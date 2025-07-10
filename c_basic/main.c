f1d15d4243b9fecfbaca556916a8079ac2ad14bd
Steven B <steven@botzek.com>
2025-07-10 01:49:26 -0700

    When program ends print out performance metrics

--- c_basic/main.c
+++ c_basic/main.c
@@ -6,13 +6,19 @@
 
 #define MAX_TICKS 10000
 #define TICK_USEC 6000
+long tick_times[MAX_TICKS];
+
+int cmp_long(const void *a, const void *b) {
+    long va = *(const long *)a;
+    long vb = *(const long *)b;
+    return (va > vb) - (va < vb); // avoids overflow
+}
 
 int main() {
     City city;
 
     city_init(&city);
 
-    long last_elapsed = 0;
     struct timeval start, end;
     for (int tick = 0; tick < MAX_TICKS; ++tick) {
         printf("\033[H\033[J");
@@ -20,9 +26,10 @@ int main() {
         int day = tick / TICKS_PER_DAY + 1;
         int hour = TICK_HOUR(tick);
         int minute = TICK_HOUR_MINUTE(tick);
+        long tick_time = tick == 0 ? 0 : tick_times[tick-1];
 
-        printf("Day %d %2d:%02d, Tick %d, Last Elapsed %ld%s, Last Moved %d/%d %d%%\n", day, hour, minute, tick,
-               last_elapsed >= 1000 ? last_elapsed / 1000 : last_elapsed, last_elapsed >= 1000 ? "ms" : "us",
+        printf("Day %d %2d:%02d, Tick %d, Tick Time %ld%s, Last Moved %d/%d %d%%\n", day, hour, minute, tick,
+               tick_time >= 1000 ? tick_time / 1000 : tick_time, tick_time >= 1000 ? "ms" : "us",
                city.num_moved, city.num_wanted_move, (city.num_wanted_move == 0 ? 100 : (city.num_moved * 100 / city.num_wanted_move)));
         city_draw(&city);
 
@@ -33,9 +40,25 @@ int main() {
         long elapsed = (end.tv_sec - start.tv_sec) * 1000000L
                      + (end.tv_usec - start.tv_usec);
         long sleep_for = TICK_USEC - elapsed;
-        last_elapsed = elapsed;
+        tick_times[tick] = elapsed;
         if (sleep_for > 0)
             usleep(sleep_for);
     }
+
+    qsort(tick_times, MAX_TICKS, sizeof(long), cmp_long);
+    long p50 = tick_times[MAX_TICKS * 50 / 100];
+    long p95 = tick_times[MAX_TICKS * 95 / 100];
+    long p99 = tick_times[MAX_TICKS * 99 / 100];
+    long max = tick_times[MAX_TICKS - 1];
+
+    double sum = 0;
+    for (int i = 0; i < MAX_TICKS; ++i) {
+        sum += (double)tick_times[i];
+    }
+    long avg = sum / MAX_TICKS;
+
+    printf("Avg: %ld us | P50: %ld | P95: %ld | P99: %ld | Max: %ld\n",
+        avg, p50, p95, p99, max);
+
     return 0;
 }
